4c6f04c26fa890dd7e992ad89fa41ca276a8192e
benpturner <2518196+benpturner@users.noreply.github.com>
2020-06-25 17:01:44 +0100

    Updated kill-implant

--- poshc2/server/database/DBSQLite.py
+++ poshc2/server/database/DBSQLite.py
@@ -136,7 +136,7 @@ def get_c2server_all():
     c = conn.cursor()
     c.execute("SELECT * FROM C2Server")
     result = c.fetchone()
-    return C2(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
+    return C2(result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9],
     result[10], result[11], result[12], result[13], result[14], result[15], result[16], result[17],
     result[18], result[19], result[20], result[21], result[22])
 
@@ -412,7 +412,7 @@ def get_implantbyid(implantId):
     result = c.fetchone()
     if result:
         return Implant(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], result[8], 
-        result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16]))
+        result[9], result[10], result[11], result[12], result[13], result[14], result[15], result[16])
     else:
         return None
 
